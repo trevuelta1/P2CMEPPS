8c0ca545b553c1174db5bcb9a81be1f708c54507
usuario <usuario@pc21-087>
2023-10-03 16:36:16 +0200

    v0.1 (clase test añadida)

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,5 +1,21 @@
 package calculadora;
 
 public class Calculadora {
-
+	public static int suma(int a, int b) {
+		return (a + b);
+	}
+	
+	public static int resta(int a, int b) {
+		return (a - b);
+	}
+	
+	public static int multiplica(int a, int b) {
+		return (a * b);
+	}
+	
+	public static int divide(int a, int b){
+		if(b != 0) {
+			return (a / b);
+		} else return -1;
+	}
 }
---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSuma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplica() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivide() {
+		fail("Not yet implemented");
+	}
+
+}
